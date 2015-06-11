d3b5e7711abd40eb102fd06ee6701e420e51f1d3
cghil <cghilarducci@gmail.com>
2015-06-11 14:37:20 -0700

    iterative palindrome

--- palindrome_recursion.rb
+++ palindrome_recursion.rb
@@ -17,3 +17,26 @@ end
 
 p palindrome?('rotor')
 p palindrome?('motor')
+
+def palindrome_iterative(str)
+	word = str
+	frontIndex = 0
+	size = word.length - 1
+	answer = true
+	while size >=1
+		front_letter = word[frontIndex]
+		back_letter = word[-1]
+		if front_letter == back_letter
+			word[frontIndex] = ''
+			word.chop!
+			size = word.length - 1
+		else
+			answer = false
+			break
+		end
+	end
+	answer
+end
+
+p palindrome_iterative('rotor')
+p palindrome_iterative('motor')
